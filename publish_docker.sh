fb78a649704fd54cbd60c21f1d297b20a6c7d3c6
Pablo Bastidas <pablo.bastidas@leaseplan.com>
2021-01-20 16:41:04 +0100

    publish_docker.sh fixation

--- publish_docker.sh
+++ publish_docker.sh
@@ -1,3 +1,3 @@
-./mvnw package -Dquarkus.package.type=fast-jar
+./mvnw clean package -Dquarkus.package.type=fast-jar
 docker build -f src/main/docker/Dockerfile.fast-jar -t pablobastidasv/ferrosuministros .
 docker image push pablobastidasv/ferrosuministros:latest
